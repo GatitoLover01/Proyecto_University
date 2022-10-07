561613392326a7bedf327c246f80f0deb31c2c8a
Adry Moisés <Adry Moiss@DESKTOP-AJ5RSS3>
2022-10-07 14:29:59 -0500

    Prime commit 07/10/2022

--- src/proyecto_universidad/Muestra.java
+++ src/proyecto_universidad/Muestra.java
@@ -42,7 +42,12 @@ public class Muestra extends javax.swing.JFrame {
             }
         });
 
-        jTextField1.setText("HOLIS");
+        jTextField1.setText("HOLI PANITAS");
+        jTextField1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextField1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -80,6 +85,10 @@ public class Muestra extends javax.swing.JFrame {
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jTextField1ActionPerformed
 
     /**
      * @param args the command line arguments
