a78182d7845ff505eb9c6dd95fbe4ebb544af073
erick <erick@LAPTOP-T6J5A63D>
2022-10-07 19:36:52 -0500

    PUSE HOLIS

---
+++ src/proyecto_universodad/DAL/conexion.java
@@ -0,0 +1,17 @@
+package proyecto_universodad.DAL;
+import java.sql.*;
+import javax.swing.*;
+/**
+ *
+ * @author erick
+ */
+
+public class conexion {
+    
+    Connection conn = null;
+    
+    public Connection conexion(){
+        
+        return conn;
+    }
+}
