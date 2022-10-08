aaebb3701eb454fdae298694796c484e2ddc42f7
erick <erick@LAPTOP-T6J5A63D>
2022-10-08 17:38:20 -0500

    Agregé un nuevo package para las imagenes. Ahí agregé una final y las otras de prueba.
    Quedó listo el modulo de login.
    Y modifique para que al correr el programa, inicie con el login

--- src/proyecto_universidad/Proyecto_Universidad.java
+++ src/proyecto_universidad/Proyecto_Universidad.java
@@ -1,9 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package proyecto_universidad;
+import proyecto_universidad_GUI.Login;
 
 /**
  *
@@ -15,7 +11,8 @@ public class Proyecto_Universidad {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Login p = new Login();
+        p.setVisible(true);
     }
     
 }
