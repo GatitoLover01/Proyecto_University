35fb9ae82dd62e19b7fb7807f205b33075217aff
erick <erick@LAPTOP-T6J5A63D>
2022-10-09 20:48:37 -0500

    Agregé paquete .DAL 09/10/2022

---
+++ src/proyecto_universidad/DAL/conexion.java
@@ -0,0 +1,73 @@
+package proyecto_universidad.DAL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+/**
+ *
+ * @author erick
+ */
+public class conexion {
+    
+    String strConexionDB = "jdbc:mysql://db4free.net:3306/universidad_proy";
+    String user = "gatitolover01";
+    String pass = "equipoGl01";
+    Connection conn = null;
+    
+    public conexion(){
+        try {    
+            //MySql
+            Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+            
+            //MySql
+            conn = (Connection)DriverManager.getConnection(strConexionDB, user, pass);
+            
+            System.out.println("Conexion establecida");
+            
+        } catch (Exception e) {
+            System.out.println("Conexion erronea " + e);
+        }
+    }
+    
+    public int ejecutarSentenciaSQL(String strSentenciaSQL){//Este metodo sirve para agregar datos a la DB
+        try {
+            PreparedStatement pstm = conn.prepareStatement(strSentenciaSQL);
+            pstm.execute();
+            return 1;
+        } catch (Exception e) {
+            System.out.println(e);
+            return 0;
+        }
+        
+        //objConexion.ejecutarSentenciaSQL("INSERT INTO `usuarios`(`Iduser`, `nickname`, `pass`, `nombre`, `email`, `tel`) VALUES (2,'user2',312654,'Meydi','laMeyz@gmail.com','9996324521')");         
+    }
+    
+    public ResultSet consultarRegistros(String strSentenciaSQL){//Este metodo obtiene datos de DB
+        try {
+            PreparedStatement pstm = conn.prepareStatement(strSentenciaSQL);
+            ResultSet respuesta = pstm.executeQuery();
+            
+            return respuesta;
+            
+        } catch (Exception e) {
+            System.out.println(e);
+            return null;
+        }
+        
+        /*  ESTO IRA EN EL BOTON
+        try {
+            ResultSet resultado = objConexion.consultarRegistros("SELECT * FROM usuarios");
+            while(resultado.next()){
+                System.out.println(resultado.getInt("Iduser"));
+                System.out.println(resultado.getString("nickname"));
+                System.out.println(resultado.getString("pass"));
+                System.out.println(resultado.getString("nombre"));
+                System.out.println(resultado.getString("email"));
+                System.out.println(resultado.getString("tel"));
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }*/    
+    }
+}
