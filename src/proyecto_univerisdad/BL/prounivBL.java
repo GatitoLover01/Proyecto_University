e6181ab605688bc97fe567ac0cd154d0806a1f51
erick <erick@LAPTOP-T6J5A63D>
2022-10-09 20:49:33 -0500

    Agregé paquete .BL 09/10/2022

---
+++ src/proyecto_univerisdad/BL/prounivBL.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto_univerisdad.BL;
+
+/**
+ *
+ * @author erick
+ */
+public class prounivBL {
+    String matricula, contraseña;
+    int categoria;
+
+    public int getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(int categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+    
+}
